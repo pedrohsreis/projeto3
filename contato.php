30557f8fb9e1ebcab67999d59247cd19f5ab6737
zChristianGTz <christianscoralich@hotmail.com>
2017-11-07 22:02:15 -0200

    Enviando pagina produtos php

--- contato.php
+++ contato.php
@@ -8,29 +8,29 @@
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
-
 </head>
 
-    <?php include 'cabecalho1.0.html'; ?>
+    <?php include 'cabecalho1.0.html'; ?> 
+    <?php include 'pesquisa.html'; ?> 
 <main>
-<br><br><div class="container">
+<br><br><div class="container"><!---mantem afastado da borda da tela--->
     <div class="row">
         <div class="col-md-7">
-            <div class="well well-sm">
+            <div class="well well-sm"><!---fundo em formato de caixa cinza--->
                <h1>Contatos</h1><br><br>
-                <form  method="post" action="sucesso.php">
+                <form action="contato_sucess.php">
                     <div class="row">
                         <div class="col-md-6">
 
                             <label for="nome">Nome</label>
                             <input type="text" name="nome" class="form-control" id="nome" placeholder="Insira seu nome" required="required" /><br>
-                                
+                                <!---class="form-control" / para deixar a caixa de texto do jeito que esta"--->
 
                             <label for="email">Email</label>
-                            <input type="email" name="email" class="form-control" id="email" placeholder="Insira seu email" required="required" /><br>
+                            <input type="email" class="form-control" id="email" placeholder="Insira seu email" required="required" /><br>
 
                             <label for="sobre">Assunto</label>
-                            <input type="text" name="assunto" class="form-control" id="sobre" placeholder="Insira o assunto" required="required" />
+                            <input type="text" class="form-control" id="sobre" placeholder="Insira o assunto" required="required" />
 
                         </div>
                         <div class="col-md-6">
@@ -39,7 +39,7 @@
                             <textarea name="mensagem" id="message" class="form-control" rows="7" cols="25" required="required" placeholder="Insira a sua mensagem aqui."></textarea>
 
                             <br><button title="Enviar." type="submit" class="btn btn-primary pull-right" id="enviar">Enviar</button>
-                                
+                                <!---class="btn btn-primary pull-right" / o btn btn-primary eh o botão azul do jeito que ta. ja o pull-right é para coloca-lo na direita--->
                             
                         </div>
                     </div>
@@ -49,7 +49,7 @@
         <div class="col-md-5">
             <form>
                 <legend><h3 align=center><b>Outras formas de contatos</b></h3></legend>
-                    
+                    <!---legend / adiciona a linha abaixo do nome--->
 
                 
                 <label><a href="#"><img class="img-responsive" id="twitter" title="Página da empresa no Twitter." src="img/Twitter.png" alt="" width="70" height="70"></a></label>
