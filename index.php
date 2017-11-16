87bea6f4ec4d1e5a925b2d0d96228ac20bf19608
zChristianGTz <christianscoralich@hotmail.com>
2017-11-16 10:45:06 -0200

    Enviando copia atual da master

--- index.php
+++ index.php
@@ -1,35 +1,20 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
-	<title>teste-Felicidade Imóveis</title>
-	<meta name="description" content="Descrição 156 carcteres">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Felicidade Imóveis</title>
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
 	<link rel="stylesheet" href="css/home.css">
-	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 
 </head>
 <body>
 	<!--cabeçalho-->
-	<header class="cabecalho">
-		<img class="img-responsive" id="logo" src="img/logo3.png" alt="">
-		<img src="img/cabecalho4.png" alt="">
-		<nav class="menu">
-			<ul class="nav nav-pills nav-justified">
-				<li><a href="index.html">Home</a></li>
-				<li><a href="../Christian/index.html">Produtos</a></li>
-				<li><a href="../Gabriel/quemsomos/index.gabriel.html">Quem somos</a></li>
-				<li><a href="../Mirian/mirian.php">Localização</a></li>
-				<li><a href="#">Contato</a></li>
-				<li><a href="#">Login</a></li>
-			</ul>
-		</nav>
+	<header>
+		<?php include 'cabecalho.php'; ?>
 	</header>
-
 	<!--slide-->
 	<section>
 		<div id="slide" class="carousel slide" data-ride="carousel">
@@ -43,20 +28,20 @@
 
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img class="img" src="img/casa1.jpg" alt="casa1">
+					<img class="img" src="img/imgslide.jpg" alt="casa1">
 					<div class="carousel-caption">
 						<h1>Residência dos Sonhos</h1>
 						<h3>Acordar na paz do interior</h3>
 					</div>
 				</div>
 				<div class="item">
-					<img src="img/casa2.jpg" alt="casa2">
+					<img src="img/imgslide2.jpg" alt="casa2">
 					<div class="carousel-caption">
 						<h1>segunda casa</h1>
 					</div>
 				</div>
 				<div class="item">
-					<img src="img/casa3.jpg" alt="casa3">
+					<img src="img/imgslide3.jpg" alt="casa3">
 					<div class="carousel-caption">
 						<h1>terceira casa</h1>
 					</div>
@@ -73,41 +58,12 @@
 				<span class="sr-only">Next</span>
 			</a>
 		</div>
-		<script
-		src="https://code.jquery.com/jquery-2.2.4.min.js"
-		integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-		crossorigin="anonymous"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>
+		
 	</section>
 	<div class="space-ten"></div>
 	<div class="space-ten"></div>
 	<section><!---pesquisa-->
-		<div class="w3-row-padding">
-        <div class="w3-col m4">
-            <input class="w3-input" style="width:100%" type="text" name="Nome"  size="40" placeholder="Digite um bairro ou cidade. ">
-        </div>
-        <div class="w3-col m1 ">
-            <form method="post" >
-                <select  class="w3-select" name="tipoImovel">
-                    <option value="Casa">Casa</option>
-                    <option value="Apto">Apartamento</option>
-                    <option value="SalaComercial">Salas Comerciais</option>
-                    <option value="Galpao">Galpão</option>
-                    <option value="Chac">Chacara</option>
-                    <option value="L/t">Lote/Terrono</option>
-                </select>
-            </div>
-            <div class="w3-col m1 " >
-                <form method="post" >
-                    <select  class="w3-select" name="compraroualugar">
-                        <option value="Comprar">Comprar</option>
-                        <option value="alugar">Alugar</option>
-                    </select>
-                </div>
-                <div class="w3-col m2" >
-                    <input style="width: 50%" class="w3-btn w3-signal-black w3-large w3-round-large" type="submit" value="Buscar" >
-                </div>
-            </div>
+		<?php include 'pesquisa.php'; ?>
 	</section>	
 	<div class="space-ten"></div>
 	<div class="space-ten"></div>
@@ -126,7 +82,7 @@
 				</div>
 				<div class="col-md-4">
 					<div class="thumbnail">
-						<a type="button" class="btn btn-primary" data-toggle="modal" data-target="#product_view2"><i class="fa fa-search"></i><img src="img/quickcasa2.jpg" alt="" class="img-responsive"></a>
+						<a type="button" class="btn btn-primary" data-toggle="modal" data-target="#product_view2"><img src="img/quickcasa2.jpg" alt="" class="img-responsive"></a>
 						<div class="caption">
 							<h4>CASA 2</h4>
 							<p>Dois quartos, todos com ar-condicionado e ventilador de teto, podendo conter uma cama de casal e um beliche, e claro, temos armadores para rede em todos os quartos.</p>
@@ -135,7 +91,7 @@
 				</div>
 				<div class="col-md-4">
 					<div class="thumbnail">
-						<a type="button" class="btn btn-primary" data-toggle="modal" data-target="#product_view3"><i class="fa fa-search"></i><img src="img/quickcasa3.jpg" alt="" class="img-responsive"></a>
+						<a type="button" class="btn btn-primary" data-toggle="modal" data-target="#product_view3"><img src="img/quickcasa3.jpg" alt="" class="img-responsive"></a>
 						<div class="caption">
 							<h4>CASA 3</h4>
 							<p>Dois quartos, todos com ar-condicionado e ventilador de teto, podendo conter uma cama de casal e um beliche, e claro, temos armadores para rede em todos os quartos.</p>
@@ -151,7 +107,7 @@
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
-						<a href="index.html" data-dismiss="modal" class="class pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+						<a href="index.php" data-dismiss="modal" class="class pull-right"><span class="glyphicon glyphicon-remove"></span></a>
 						<h3 class="modal-title">Dê o primeiro passo para realizar o seu sonho</h3>
 					</div>
 					<div class="modal-body">
@@ -173,7 +129,7 @@
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
-						<a href="index.html" data-dismiss="modal" class="class pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+						<a href="index.php" data-dismiss="modal" class="class pull-right"><span class="glyphicon glyphicon-remove"></span></a>
 						<h3 class="modal-title">Dê o primeiro passo para realizar o seu sonho</h3>
 					</div>
 					<div class="modal-body">
@@ -195,7 +151,7 @@
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
-						<a href="index.html" data-dismiss="modal" class="class pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+						<a href="index.php" data-dismiss="modal" class="class pull-right"><span class="glyphicon glyphicon-remove"></span></a>
 						<h3 class="modal-title">Dê o primeiro passo para realizar o seu sonho</h3>
 					</div>
 					<div class="modal-body">
@@ -214,32 +170,7 @@
 			</div>
 		</div>
 	</section>
-	<div class="space-ten"></div>
-	<div class="space-ten"></div>		
-		<footer id="rodape" class="mt-5">
-    <div class="container-fluid" id="container-rodape">
-        <div class="container" >
-            <div class="row p-2  text-center">
-                <div class="col-lg-8 col-md-12 mt-auto mb-auto">
-                    <p class="texto"><br><br>© Copyright 2017 - Felicidade Imóveis
- 
-                    </p>
-                </div>
-
-                <div class="col-md-12 col-lg-4 text-center" >
-
-                    <br><!--<iframe  class="d-block mr-auto ml-auto" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fsouzagomesimoveis%2F&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
-                            width="340" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br>-->
-                    <ul lass="loirinhausaclasse"><li><a href="http://facebook.com/"><i class="fa fa-facebook "></i></a></li>
-                        <li><a href="http://twitter.com/"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="http://plus.google.com/"><i class="fa fa-google-plus"></i> </a></li>
-                        <li><a href="http://instagram.com/"><i class="fa fa-instagram"></i> </a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-    </div>
+	<?php include 'rodape.php'; ?>
 </footer>
 
 </body>
--- quem-somos.php
+++ quem-somos.php
@@ -1,26 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link rel="stylesheet" href="css/reset.css">
-     <link rel="stylesheet" type="text/css" href="css/quemSomos.css">
-     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-     <link rel="icon" href="img/felicidade.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" type="text/css" href="css/quemSomos.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="icon" href="img/felicidade.png">
 
-     <title>Felicidade Imóveis--Quem Somos</title>
+  <title>Felicidade Imóveis--Quem Somos</title>
 
 </head>
-
+<header>
+  <?php include('cabecalho.php'); ?>
+</header>
 
 <body>
 
     <main>
+      <br>
+      <?php include("pesquisa.php"); ?>
 
-<div class="container">
+<div class="principal">
+	
 <div class="card" style="width: 20rem;">
-  <img class="card-img-top" src="assets/img/icone-missao3.png" alt="Card image cap">
+  <img class="card-img-top" src="img/icone-missao3.png" alt="Card image cap">
   <div class="card-block">
     <h3 class="card-title">Missão</h3>
     <p class="card-text"><strong>Expor e ajudar a encontrar imóveis na cidade de juiz de fora prezando a satisfacão e o contentamento do cliente sempre!</strong></p>
@@ -28,7 +33,7 @@
 </div>
 
 <div class="card" style="width: 20rem;">
-  <img class="card-img-top" src="assets/img/icone-visao3.png" alt="Card image cap">
+  <img class="card-img-top" src="img/icone-visao3.png" alt="Card image cap">
   <div class="card-block">
     <h3 class="card-title">Visão</h3>
     <p class="card-text"><strong>Ser uma empresa do ramo de imóveis reconhecida e respeitada no mercado almejando causar grande impacto em tal mercado e se tornar uma referência no setor na cidade de Juiz de Fora.</strong></p>
@@ -36,7 +41,7 @@
 </div>
 
 <div class="card" style="width: 20rem;">
-  <img class="card-img-top" src="assets/img/icone-valores3.png" alt="Card image cap">
+  <img class="card-img-top" src="img/icone-valores3.png" alt="Card image cap">
   <div class="card-block">
     <h3 class="card-title">Valores</h3>
     <p class="card-text"><strong>Comprometimento,maestria e respeito.</strong></p>
@@ -48,7 +53,9 @@
 
 </main>
 <br><br>
-<?php include 'rodape.html' ?>;
+<footer>
+<?php include 'Rodape.php' ?>;
+</footer>
 
 </body>
 </html>
