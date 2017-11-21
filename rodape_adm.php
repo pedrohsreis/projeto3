b5f9b43c24b0eb6bfeec14e65251a8e66e6561a6
DarlanHenrique <darlan_jf@hotmail.com>
2017-11-21 09:34:19 -0200

    Versao 1.0 da pagina inicial administrativa, cabecalho e rodape administrativo

---
+++ rodape_adm.php
@@ -0,0 +1,14 @@
+<html lang="pt-br">
+    <head>
+    <meta charset="UTF-8">
+    <meta name ="Descriçao" content="Rodape">
+    <link rel="stylesheet" href="css/CssRodape.css">
+    <link rel="stylesheet"  type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+    </head>
+
+
+<footer id="rodape" class="mt-5">
+    <p class="texto" align='center'><br><br>© Copyright 2017 - Felicidade Imóveis</p>
+</footer>
