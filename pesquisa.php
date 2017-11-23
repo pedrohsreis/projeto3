b8ac87c386a52841f5243918354b5ef4d652d291
Ramon Oliveira <ramonomello@gmail.com>
2017-11-23 19:45:26 -0200

    tirando link da pesquisa

--- pesquisa.php
+++ pesquisa.php
@@ -6,8 +6,6 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
-    <link rel="stylesheet" href="css/pesquisa.css">
-
     <title></title>
 </head>
 <body>
