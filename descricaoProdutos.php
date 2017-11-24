163c618d91534675f24429aad7651bd8c05107f9
Ramon Oliveira <ramonomello@gmail.com>
2017-11-24 03:18:10 -0200

    feitas alteracoes na pagina produtos

--- descricaoProdutos.php
+++ descricaoProdutos.php
@@ -1,30 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="utf-8">
     <title>Descrição dos produtos</title>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto+Slab:400,700|Pacifico' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/normalize.css">
-
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/cabecalho1.0.css">
-
-    <link rel="stylesheet" type="text/css" href="CSS/produtos.css">
-
-
-    <link rel="stylesheet" href="CssRodape.css">
-    <link rel="stylesheet"  type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/CssRodape.css">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
-<script src="js/jquery.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-
-    <?php
-    include 'cabecalho.php';
-    include 'pesquisa.php';
+    <header>
+        <?php
+        include 'cabecalho.php';
+        include 'pesquisa.php';
+        ?>
+    </header>
+    <?php 
+    require 'config.php';
+    require 'connection.php';
+    require 'database.php';
     ?>
-<?php include 'Rodape.php'; ?>
+    <?php 
+    if(array_key_exists("id", $_GET)){ 
+    $id = $_GET['id'];
+    } 
+     ?>
+    <?php $result_imovel =  DBRead('imovel', "WHERE idimovel = $id order by idimovel")?>
+    <?php foreach ($result_imovel as $v) :  ?>
+    <section class="container">
+        <div class="col-md-12">
+        <div class="col-md-2"> <img src="img/<?php echo $v['foto'] ?>" alt="Foto de exibição"></div>
+        <div class="col-md-10"><?php echo $v['nome'] ?></div>
+        </div>
+
+    </section>
+    <?php endforeach; ?>
+    <footer>
+        <?php include 'Rodape.php'; ?>
+    </footer>
 </body>
 </html>
--- produtos.php
+++ produtos.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Pagina produtos</title>
+    <link rel="icon" href="img/icone.png">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto+Slab:400,700|Pacifico' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/normalize.css">
 
@@ -13,7 +14,7 @@
     <link rel="stylesheet" type="text/css" href="CSS/produtos.css">
 
 
-    <link rel="stylesheet" href="CssRodape.css">
+    <link rel="stylesheet" href="css/CssRodape.css">
     <link rel="stylesheet"  type="text/css" href="bootstrap/css/bootstrap.min.css">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
@@ -24,11 +25,10 @@
 
 
 
-    <script src="js/jquery.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
 
     <?php
     include 'cabecalho.php';
+    echo "<br>";
     include 'pesquisa.php';
     echo'<br><br>';
     include 'conexao.php';
@@ -57,7 +57,7 @@
         if ($comprarOuAlugar == "Alugar")
             $comprarOuAlugar = 2;
         ?><section class="container">
-            
+
             <?php
             $sql = mysqli_query($conn, "SELECT * FROM imovel where nome like '%$nome%' OR idimovel = '$nome' OR descricao like '%$nome%' "
                 . "AND TipoImovel='$TipoImovel' AND tipo_venda = '$comprarOuAlugar'");
@@ -83,82 +83,45 @@
         <?php
     }
     ?>
+    <?php 
+    require 'config.php';
+    require 'connection.php';
+    require 'database.php';
+    ?>
 
+    <?php $result_imovel =  DBRead('imovel', 'order by idimovel')?>
+    <?php if ($result_imovel) : ?>
+      <section>
+          <?php foreach ($result_imovel as $v) :  ?>
 
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <a href="descricaoProdutos.php?id=<?php echo $v['idimovel']?>" type="link" class="btn btn-primary">
+                                <img src="img/<?php echo $v['foto'] ?>" alt="Foto de exibi��o">
+                            </a>
+                            <div class="caption">
+                                <h4>
+                                    <p class="text-center"><?php echo $v['nome'] ?></p>
+                                </h4>
 
-    <section class="container">        
-        <figure class="thumbnail projeto-responsivo">
-            <?php
-            $sqp = mysqli_query($conn, "SELECT idimovel FROM imovel order by idimovel DESC limit 1");
-            $x = mysqli_fetch_object($sqp);
-            $b = $x->idimovel;
-            $sql = mysqli_query($conn, "SELECT * FROM imovel where idimovel = $b");
-
-            while ($usuario = mysqli_fetch_object($sql)) {
-
-                echo "<img src='img/" . $usuario->foto . "' alt='Foto de exibição' />";
-                ?>
-                <figcaption class="caption">
-                    <h3><?php echo $usuario->nome; ?></h3>
-                    <p><?php echo $usuario->descricao; ?></p>
-
-
-                    <?php
-                }
-                ?>
-                <a href="descricaoProdutos.php">Saiba Mais</a>
-            </figcaption>
-        </figure>
-
-
-
-
-        <figure class="thumbnail projeto-responsivo">
-
-            <?php
-            $b = $x->idimovel - 1;
-            $sql = mysqli_query($conn, "SELECT * FROM imovel where idimovel = $b");
-            while ($usuario = mysqli_fetch_object($sql)) {
-
-
-                echo "<img src='img/" . $usuario->foto . "' alt='Foto de exibição' />";
-                ?>
-                <figcaption class="caption">
-                    <h3><?php echo $usuario->nome; ?></h3>
-                    <p><?php echo $usuario->descricao; ?></p>
-                    <?php
-                }
-                ?>
-                <a href="descricaoProdutos.php">Saiba Mais</a>
-            </figcaption>
-        </figure>
-
-
-
-        <figure class="thumbnail projeto-responsivo">
-
-            <?php
-            $b = $x->idimovel - 2;
-            $sql = mysqli_query($conn, "SELECT * FROM imovel where idimovel = $b");
-            while ($usuario = mysqli_fetch_object($sql)) {
-
-
-                echo "<img src='img/" . $usuario->foto . "' alt='Foto de exibição' />";
-                ?>
-                <figcaption class="caption">
-                    <h3><?php echo $usuario->nome; ?></h3>
-                    <p><?php echo $usuario->descricao; ?></p>
-                    <?php
-                }
-                ?>
-                <a href="descricaoProdutos.php">Saiba Mais</a>
-            </figcaption>
-        </figure>
-
-
-
+                            </div>               
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </section>
-    <?php include 'Rodape.php'; ?>
+
+<?php else : ?>
+    <tr>
+      <td colspan="6">Nenhum registro encontrado.</td>
+  </tr>
+<?php endif; ?>
+<footer>
+ <?php include 'Rodape.php'; ?>
+</footer>
 </body>
 
 </html>
