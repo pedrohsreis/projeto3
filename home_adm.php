2f2aff7dd6a4d562090405f723f3805475a4f5cd
DarlanHenrique <darlan_jf@hotmail.com>
2017-11-23 23:03:22 -0200

    Atualização da Pagina inicial administrativa. Versão Final.

--- home_adm.php
+++ home_adm.php
@@ -1,15 +1,24 @@
-    	<?php
-
+<?php
             session_start();
 
             if(!isset($_SESSION['nome']) && !isset($_SESSION['senha']))
             {
                 header("location: Login.php");
             }
+               
+                require 'config.php';
+                require 'connection.php';
 
+                           
+                $link = DBConnect();
+                $sqlc = mysqli_query($link, "SELECT * FROM contato");
+                $totalc = mysqli_num_rows($sqlc);
+                $sqlp = mysqli_query($link, "SELECT * FROM imovel");
+                $totalp = mysqli_num_rows($sqlp);
+                
     		include("conexao.php");
     		include("banco-contato.php");
-    	?>
+?>
     <head>
         <meta charset="utf-8">
         <title>Home Administrativa</title>
@@ -22,17 +31,17 @@
         <br><h1 align="center">Bem vindo a área administrativa.</h1>
         <br><p align='center'>Aqui nessa área você consegue ver a quantidade de produtos cadastrados e a quantidade de mensagens recebidas pela página contatos.</p><br>
         <div class="col-md-6">
-            <div class="well well-sm">
-                <legend><h4 align='center'>Quantidade de Produtos cadastrados</h4></legend><br>
-                <p>Temos 0 produtos cadastrados no site.</p>
+            <div class="well well-sm" align='center'>
+                <legend><h4>Quantidade de Produtos cadastrados</h4></legend><br>
+                <p>Temos <?php echo $totalp ?> produtos cadastrados no site.</p>
                 <p>Clique <a href="admProdutos.php">aqui</a> para acessar a página de cadastros dos produtos.</p>
             </div>
         </div>
         <div class="col-md-6">
-            <div class="well well-sm">
-                <legend><h4 align='center'>Quantidade de Mensagens Recebidas</h4></legend><br>
-                <p>Temos 0 mensagens recebidas no site.</p>
-                <p>Clique <a href="cadastro.php">aqui</a> para acessar a página de administração dos contatos.</p>
+            <div class="well well-sm" align='center'>
+                <legend><h4 >Quantidade de Mensagens Recebidas</h4></legend><br>
+                <p>Temos <?php echo $totalc ?> mensagens recebidas no site.</p>
+                <p>Clique <a href="contato-lista.php">aqui</a> para acessar a página de administração dos contatos.</p>
             </div>
         </div>
 
